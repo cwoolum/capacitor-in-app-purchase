8d912b70f17e70f77b267f72f9308a3d521bb8e2
Chris Woolum <christopher.woolum@gmail.com>
2020-03-23 11:10:59 -0700

    fix web

--- src/web.ts
+++ src/web.ts
@@ -8,26 +8,23 @@ export class InAppPurchaseWeb extends WebPlugin implements InAppPurchasePlugin {
       platforms: ['web']
     });
   }
-  consumePurchase(options: { purchaseToken: string; }): Promise<void> {
+  consumePurchase(purchaseToken: string): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  subscribe(options: { type: SkuType; receipt: String; }): Promise<{ transactionId: String; productId: String; token: String; }> {
+  subscribe(type: SkuType, receipt: String): Promise<{ transactionId: String; productId: String; token: String; }> {
     throw new Error("Method not implemented.");
   }
   restorePurchases(): Promise<{ data: import("./definitions").PurchaseDetail[]; }> {
     throw new Error("Method not implemented.");
   }
-  getSkuDetails(options: { skus: string[]; skuType: SkuType; }): Promise<{ data: import("./definitions").SkuDetail[]; }> {
+  getSkuDetails(skus: string[], skuType: SkuType): Promise<{ data: import("./definitions").SkuDetail[]; }> {
     throw new Error("Method not implemented.");
   }
   initialize(): Promise<void> {
     throw new Error("Method not implemented.");
   }
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
-  }
+
 }
 
 const InAppPurchase = new InAppPurchaseWeb();
